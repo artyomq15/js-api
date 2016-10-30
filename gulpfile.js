189d8fd1ebbc913896189cacd68b2d5fd83fa1e4
artyomstankevich <artyom.stan0905@gmail.com>
2016-10-30 16:10:56 +0300

    change api from vk to mixcloud

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var gulp = require ('gulp');
-
-gulp.task('hello', function(callback){
-    console.log("Hello");
-    callback();
-});
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,10 @@
 'use strict';
-// https://oauth.vk.com/blank.html#access_token=78b76241de6f715cd504d6db0ff67e0f0e091007142eec6ec04238e56327063e814c5b2f5778bd07783ed&expires_in=86400&user_id=81238799
 
-var token = '78b76241de6f715cd504d6db0ff67e0f0e091007142eec6ec04238e56327063e814c5b2f5778bd07783ed'
-//   'https://api.vk.com/method/users.search?from_list = friends&user_id=81238799&name_case=Nom&offset=10&count_10&access_token=' + token + '&v=5.59'
 $('#loadingDiv').show();
-let getInfo = () => {
+let getJSON = () => {
     $.ajax({
         type: 'GET',
-        url:'https:api.vk.com/method/newsfeed.search?&q=БГУ&extended=0&start_time=1477227600&v=5.59',
-        headers: {
-            'client-id': token
-        },
+        url:'https://api.mixcloud.com/new/',
         dataType: 'JSONP',
         success:function(channel) {
             console.log(channel);
@@ -24,6 +18,6 @@ let getInfo = () => {
 }
 
 $(document).ready(function () {
-    getInfo();
+    getJSON();
     setInterval(getInfo, 10000);
 })
