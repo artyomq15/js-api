e41602f35e0be2e30265be25caba0f22d401a0ef
artyomstankevich <artyom.stan0905@gmail.com>
2016-11-14 01:12:01 +0300

    final v

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,42 @@
 'use strict';
 
 $('#loadingDiv').show();
-let getJSON = () => {
+
+function getJSON() {
     $.ajax({
         type: 'GET',
         url:'https://api.mixcloud.com/new/',
-        dataType: 'JSONP',
+        dataType: 'JSON',
         success:function(channel) {
             console.log(channel);
             var template = Handlebars.compile( $('#template').html()  );
-            $('.updates').empty().append( template(channel)  );
+            $('.updates').empty().show().append( template(channel)  );
         },
-        error:function() {
-            console.log("error");
+        error:function(error) {
+            console.log(error.status+":"+error.responseText);
+            $('.updates').empty().append('<div><h1>Ooops...Something went wrong :(</h1></div>');
+            //alert("Error:" + error.status + error.responseText);
+        },
+        statusCode:{
+            0: function(){
+                $('.updates').append('<div><h3>Connection lost. Please check your Internet connection!</h3></div>');
+            },
+            403: function(){
+                $('.updates').append('<div><h3>Forbidden</h3></div>');
+            },
+            404:function(){
+                $('.updates').append('<div><h3>Not found</h3></div>');
+            },
+            500:function(){
+                $('.updates').append('<div><h3>Internal server error</h3></div>');
+            }
         }
     });
 }
 
-$(document).ready(function () {
-    getJSON();
-    setInterval(getJSON, 10000);
-})
+    $(document).ready(function () {
+        setTimeout(getJSON, 5000);
+        setInterval(getJSON, 10000);
+    });
+
+
