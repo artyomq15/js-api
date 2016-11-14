587835b36f0656c92d8612f47efdeacbba9209a1
artyomstankevich <artyom.stan0905@gmail.com>
2016-11-14 09:34:35 +0300

    final v2

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,46 @@
 'use strict';
 
 $('#loadingDiv').show();
-
 function getJSON() {
+
     $.ajax({
         type: 'GET',
         url:'https://api.mixcloud.com/new/',
         dataType: 'JSON',
-        success:function(channel) {
-            console.log(channel);
+        success:function(recourse) {
+            console.log(recourse);
             var template = Handlebars.compile( $('#template').html()  );
-            $('.updates').empty().show().append( template(channel)  );
+            $('.info').empty().append( template(recourse)  );
+
+            throw new Error("Error");
+
         },
         error:function(error) {
             console.log(error.status+":"+error.responseText);
-            $('.updates').empty().append('<div><h1>Ooops...Something went wrong :(</h1></div>');
+            $('.info').empty().append('<div><h1>Ooops...Something went wrong :(</h1></div>');
             //alert("Error:" + error.status + error.responseText);
         },
         statusCode:{
             0: function(){
-                $('.updates').append('<div><h3>Connection lost. Please check your Internet connection!</h3></div>');
+                $('.info').append('<div><h3>Failed to load resource. Connection lost. Please check your Internet connection!</h3></div>');
             },
             403: function(){
-                $('.updates').append('<div><h3>Forbidden</h3></div>');
+                $('.info').append('<div><h3>Forbidden</h3></div>');
             },
             404:function(){
-                $('.updates').append('<div><h3>Not found</h3></div>');
+                $('.info').append('<div><h3>Not found</h3></div>');
             },
             500:function(){
-                $('.updates').append('<div><h3>Internal server error</h3></div>');
+                $('.info').append('<div><h3>Internal server error</h3></div>');
+            },
+            504:function(){
+                $('.info').append('<div><h3>Failed to load recourse. Internal server error</h3></div>');
             }
         }
     });
 }
 
     $(document).ready(function () {
-        setTimeout(getJSON, 5000);
         setInterval(getJSON, 10000);
     });
 
